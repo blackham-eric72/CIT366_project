655d85cd7e2c08b59df8ec6038ca4e6f24cdd00a
blackerrow <erict.blackham@gmail.com>
2018-01-16 14:45:34 -0700

    1/16/2018 - completed all of the requirements for lesson01 assignment. All tests run without fail.

--- AllEventsTicketing/AllEventsTicketing/Set.js
+++ AllEventsTicketing/AllEventsTicketing/Set.js
@@ -28,33 +28,72 @@ function Set() {
 
         var resultList = [];
 
-        /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {//check for invalid inputs
+            return null;//exit and return null to indicate an error
+        }
 
-        /*-------------------------------Insert your code here -------------------------------------*/
+
+        var complimentA = this.relativeComplement(listA,listB);
+        var complimentB = this.relativeComplement(listB,listA);
+        var intersection = this.intersection(listA,listB);
+
+        for(i=0; i < complimentA.length; i++){
+            resultList.push(complimentA[i]);
+        }
+        for(i=0; i < complimentB.length; i++){
+            resultList.push(complimentB[i]);
+        }
+        for(i=0; i < intersection.length; i++){
+            resultList.push(intersection[i]);
+        }
+
+
 
         return resultList;
+
     }
 
 
     this.relativeComplement = function (listA, listB) {
-
         var resultList = [];
+        if (listA === null || listB === null) {//check for invalid inputs
+            return null;//exit and return null to indicate an error
+        }
+        if(listB == 0){
+            return listA;
+        }
 
-        /*-------------------------------Insert your code here -------------------------------------*/
+        for (var i = 0; i < listA.length; i++) {
+            var nextValue = listA[i];
 
-        /*-------------------------------Insert your code here -------------------------------------*/
-
+            for (var j = 0; j < listB.length; j++) {
+                if (listB[j] === nextValue) {
+                    break;
+                }
+                if (j == listB.length - 1){
+                    resultList.push(nextValue);
+                }//if statement
+            }//list b inner loop
+        }//list a outer loop
         return resultList;
     }
 
 
     this.symmetricDifference = function (listA, listB) {
-
+        if (listA === null || listB === null) {//check for invalid inputs
+            return null;//exit and return null to indicate an error
+        }
         var resultList = [];
 
-        /*-------------------------------Insert your code here -------------------------------------*/
+        var complimentA = this.relativeComplement(listA,listB);
+        var complimentB = this.relativeComplement(listB,listA);
 
-        /*-------------------------------Insert your code here -------------------------------------*/
+        for(i=0; i < complimentA.length; i++){
+            resultList.push(complimentA[i]);
+        }
+        for(i=0; i < complimentB.length; i++){
+            resultList.push(complimentB[i]);
+        }
 
         return resultList;
     }
