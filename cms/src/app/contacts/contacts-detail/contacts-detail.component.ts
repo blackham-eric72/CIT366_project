6787c08032af7af931e2225c96d93413e020d2ca
blackerrow <erict.blackham@gmail.com>
2018-01-25 23:26:56 -0700

    CMS project, week 02 submission

---
+++ cms/src/app/contacts/contacts-detail/contacts-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+@Component({
+  selector: 'cms-contacts-detail',
+  templateUrl: './contacts-detail.component.html',
+  styleUrls: ['./contacts-detail.component.css']
+})
+export class ContactsDetailComponent implements OnInit {
+  contact: Contact[] = [
+    new Contact(1,"Bro. Jackson","jacksonk@byui.edu","208-496-3771", "https://web.byui.edu/Directory/Employee/jacksonk.jpg",null),
+    new Contact(2,"Bro. Barzee","barzeer@byui.edu","208-496-3768", "https://web.byui.edu/Directory/Employee/barzeer.jpg",null)
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
