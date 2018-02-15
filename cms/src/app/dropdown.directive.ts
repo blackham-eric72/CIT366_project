7615dd1e6c04252c1f9972029675d9ac45cf3f52
blackerrow <erict.blackham@gmail.com>
2018-02-15 12:41:45 -0700

    2/15/18 Lesson 4 now complete

--- cms/src/app/dropdown.directive.ts
+++ cms/src/app/dropdown.directive.ts
@@ -6,7 +6,8 @@ export class DropdownDirective{
   @HostBinding('class.open') isOpen = false;
   @HostListener('click') toggleOpen(){
     this.isOpen = !this.isOpen;
-
   }
-
+  @HostListener('mouseleave') toggleClosed(){
+    this.isOpen = !this.isOpen;
+  }
 }
